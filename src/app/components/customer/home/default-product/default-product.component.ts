79fe2f14cd8cf9d1e3196b8db2cafcc2b487ae63
tuntunpandit <tuntunkumarjamui@gmail.com>
2024-06-06 11:02:07 +0530

    1. product page completed 2. category page completed3. profile page completedß

--- src/app/components/customer/home/default-product/default-product.component.ts
+++ src/app/components/customer/home/default-product/default-product.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { CurrencyPipe, SlicePipe } from '@angular/common';
 import { Product } from '../../../../models/product';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-default-product',
   standalone: true,
-  imports: [SlicePipe, CurrencyPipe],
+  imports: [SlicePipe, CurrencyPipe, RouterLink],
   templateUrl: './default-product.component.html',
   styleUrl: './default-product.component.scss',
 })
